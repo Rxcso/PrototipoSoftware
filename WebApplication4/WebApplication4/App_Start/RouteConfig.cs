d7b225d49c0462fbfde1efbb403d28575810e11b
ramonbv <simon81265@gmail.com>
2015-10-08 09:23:48 -0500

    registro corregido

--- WebApplication4/WebApplication4/App_Start/RouteConfig.cs
+++ WebApplication4/WebApplication4/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace WebApplication4
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
+            
             routes.MapPageRoute("", "{*query}", "~/Home/Index", true);
  
 
