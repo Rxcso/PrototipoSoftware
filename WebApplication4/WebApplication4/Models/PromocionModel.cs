2f77c958922d5bcd834fc0198944bbebbabeeea1
ramonbv <simon81265@gmail.com>
2015-10-17 11:44:06 -0500

    promocion delete

--- WebApplication4/WebApplication4/Models/PromocionModel.cs
+++ WebApplication4/WebApplication4/Models/PromocionModel.cs
@@ -43,4 +43,43 @@ namespace WebApplication4.Models
             [Display(Name = "Tipo Tarjeta: ")]
             public int codTipoTarjeta { get; set; }
     }
+
+    public class PromocionModel2
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Inicio:")]
+        public System.DateTime fechaIni { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Fin:")]
+        public System.DateTime fechaFin { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        [Display(Name = "Porcentaje Descuento:")]
+        public float descuento { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+
+        [Required]
+        [Display(Name = "Cantidad Adquirida")]
+        public int cantAdq { get; set; }
+
+        [Required]
+        [Display(Name = "Cantidad Comprada")]
+        public int cantComp { get; set; }
+
+        [Required]
+        [Display(Name = "Banco")]
+        public int codBanco { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo Tarjeta: ")]
+        public int codTipoTarjeta { get; set; }
+    }
+
 }
