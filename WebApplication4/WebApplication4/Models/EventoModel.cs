3958ae0331d89aa1b66ef8e392d46803490540c9
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-10-19 16:38:49 -0500

    Evento agregado a la base de datos, lista de eventos sacada desde la base de datos para mostrarla en el index

---
+++ WebApplication4/WebApplication4/Models/EventoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class EventoModel
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Nullable<System.DateTime> fecha_inicio { get; set; }
+        public Nullable<System.DateTime> fecha_fin { get; set; }
+        public string estado { get; set; }
+        public Nullable<double> monto_transferir { get; set; }
+        public Nullable<double> monto_adeudado { get; set; }
+        public string lugar { get; set; }
+        public Nullable<double> penalidadXcancelacion { get; set; }
+        public Nullable<double> penalidadXpostergacion { get; set; }
+        public Nullable<double> porccomision { get; set; }
+        public Nullable<bool> esUnico { get; set; }
+        public Nullable<int> idCategoria { get; set; }
+        public Nullable<int> idRegion { get; set; }
+        public Nullable<int> idPromotor { get; set; }
+        public Nullable<int> idOrganizador { get; set; }
+    }
+}
