9b105240f3ce74581d567d7504e8fa3234169873
ramonbv <simon81265@gmail.com>
2015-10-19 13:24:33 -0500

    avance Mantenimiento v2

--- WebApplication4/WebApplication4/Controllers/OrganizadorController.cs
+++ WebApplication4/WebApplication4/Controllers/OrganizadorController.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
     public class OrganizadorController : Controller
     {
+        private inf245netsoft db = new inf245netsoft();
         // GET: Organizador
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult RegisterOrganizador(OrganizadorModel model)
+        {
+            Organizador org = new Organizador();
+            Organizador orgL = db.Organizador.ToList().Last();
+            org.codOrg =  orgL.codOrg + 1;
+            org.codDoc = model.codDoc;
+            org.correo = model.Email;
+            org.nombOrg = model.nombre;
+            org.telefOrg = model.telefono;
+            org.tipoDoc = model.tipoDoc;
+            org.estadoOrg = "Activo";
+            db.Organizador.Add(org);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Organizador");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Organizador org = db.Organizador.Find(id);
+            //db.Regalo.Remove(regalo);
+            db.Entry(org).State = EntityState.Modified;
+            org.estadoOrg = "Inactivo";
+            db.SaveChanges();
+            //return RedirectToAction("Index", "Evento");
+            return View("Index");
+        }
+
     }
 }
--- WebApplication4/WebApplication4/Controllers/PuntoVentaController.cs
+++ WebApplication4/WebApplication4/Controllers/PuntoVentaController.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
     public class PuntoVentaController : Controller
     {
+        private inf245netsoft db = new inf245netsoft();
         // GET: PuntoVenta
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult RegisterPunto(PuntoVentaModel model)
+        {
+            PuntoVenta punto = new PuntoVenta();
+            PuntoVenta puntoL = db.PuntoVenta.ToList().Last();
+            punto.codPuntoVenta = puntoL.codPuntoVenta + 1;
+            punto.dirMAC = model.mac;
+            punto.estaActivo = true;
+            punto.ubicacion = model.ubicacion;
+            db.PuntoVenta.Add(punto);
+            db.SaveChanges();
+            return RedirectToAction("Index", "PuntoVenta");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            PuntoVenta punto = db.PuntoVenta.Find(id);
+            //db.Regalo.Remove(regalo);
+            db.Entry(punto).State = EntityState.Modified;
+            punto.estaActivo = false;
+            db.SaveChanges();
+            //return RedirectToAction("Index", "Evento");
+            return View("Index");
         }
     }
 }
