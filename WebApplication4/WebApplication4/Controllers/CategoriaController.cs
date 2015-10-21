cb2365d4b80b3ca53e6cab909f1dbc91e5acdc6f
tdachris <donchristian93@gmail.com>
2015-10-21 16:42:11 -0500

    Categoria Eventos - Delete parcial

--- WebApplication4/WebApplication4/Controllers/CategoriaController.cs
+++ WebApplication4/WebApplication4/Controllers/CategoriaController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
     public class CategoriaController : Controller
     {
+        private inf245netsoft db = new inf245netsoft();
         // GET: Categoria
         public ActionResult Index()
         {
             return View();
         }
+
+
+        public ActionResult Delete(int id)
+        {
+            Categoria categoria = db.Categoria.Find(id);            
+            db.Entry(categoria).State = EntityState.Modified;
+            categoria.activo = 0;
+            db.SaveChanges();
+            //return RedirectToAction("Index", "Evento");
+            return View("Index");
+        }
     }
+
+    
 }
