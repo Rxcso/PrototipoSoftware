f3f15db6dd49d7a571c6431ee246e22c2a32fcd9
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-10-22 16:44:34 -0500

    cambios, faltaba un ; en categoria controller o creo q la borre sin querer :V

--- WebApplication4/WebApplication4/Controllers/CategoriaController.cs
+++ WebApplication4/WebApplication4/Controllers/CategoriaController.cs
@@ -22,28 +22,28 @@ namespace WebApplication4.Controllers
             List<Categoria> listaCategoria = null;
             while (true)
             {
-                listaCategoria = db.Categoria.Where(c=>c.idCatPadre==id).ToList();
+                listaCategoria = db.Categoria.Where(c => c.idCatPadre == id).ToList();
                 if (listaCategoria == null) return;
                 else
                     for (int i = 0; i < listaCategoria.Count; i++)
-                    {                        
+                    {
                         db.Entry(listaCategoria[i]).State = EntityState.Modified;
                         listaCategoria[i].activo = 0;
                         db.SaveChanges();
-                        borrar(listaCategoria[i].idCategoria)
+                        borrar(listaCategoria[i].idCategoria);
                     }
             }
         }
 
         public ActionResult Delete(int id)
-        {            
+        {
             //borrar categoría padre
             Categoria categoria = db.Categoria.Find(id);
             db.Entry(categoria).State = EntityState.Modified;
             categoria.activo = 0;
             db.SaveChanges();
             //borrar arbol de la categoría padre
-            borrar(id);                        
+            borrar(id);
             //return RedirectToAction("Index", "Evento");
             return View("Index");
         }
@@ -52,7 +52,7 @@ namespace WebApplication4.Controllers
         {
             if (ModelState.IsValid)
             {
-                List<Categoria> listaCat = db.Categoria.AsNoTracking().Where(c => c.nombre.StartsWith(categoria.nombre) && c.activo ==1).ToList();
+                List<Categoria> listaCat = db.Categoria.AsNoTracking().Where(c => c.nombre.StartsWith(categoria.nombre) && c.activo == 1).ToList();
                 if (listaCat != null) TempData["ListaC"] = listaCat;
                 else TempData["ListaC"] = null;
                 return RedirectToAction("Index", "Categoria");
@@ -62,5 +62,5 @@ namespace WebApplication4.Controllers
         }
     }
 
-    
+
 }
