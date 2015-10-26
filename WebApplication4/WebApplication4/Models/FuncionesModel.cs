503426cb37ee1e9d9eb4243979a5edc626b76779
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-10-25 21:55:52 -0500

    agregado cosas que faltaban

---
+++ WebApplication4/WebApplication4/Models/FuncionesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class FuncionesModel
+    {
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Fecha:")]
+        public DateTime fechaFuncion { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Hora:")]
+        public DateTime horaInicio { get; set; }
+
+    }
+}
