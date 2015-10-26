9945f720f5d0d8fbcd68ace98d1bdcbc683967c3
tdachris <donchristian93@gmail.com>
2015-10-25 20:05:56 -0500

    Categoria Eventos - Delete 100 , Edit 50 <--

--- WebApplication4/WebApplication4/Models/CategoriaModel.cs
+++ WebApplication4/WebApplication4/Models/CategoriaModel.cs
@@ -35,4 +35,19 @@ namespace WebApplication4.Models
         [Display(Name = "Categoria: ")]
         public int id { get; set; }
     }
+
+    public class CategoriaEditModel
+    {
+        [Display(Name = "Nombre: ")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Descripcion: ")]
+        public string descripcion { get; set; }
+
+        [Display(Name = "Id Padre: ")]
+        public int idCatPadre { get; set; }
+
+        [Display(Name = "Activo: ")]
+        public int activo { get; set; }
+    }
 }
