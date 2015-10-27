e6d0d1754fd45e2068794b2cfa24ed797c2651f8
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-10-26 19:12:23 -0500

    validacion de funciones desde el cliente

--- WebApplication4/WebApplication4/Scripts/RegisterEvento/FuncionesHelper.js
+++ WebApplication4/WebApplication4/Scripts/RegisterEvento/FuncionesHelper.js
@@ -1,4 +1,16 @@
-﻿function format_time(date_obj) {
+﻿function validarFechaActualOMayor(fecha) {
+    var fechaS = fecha.split('-');
+    var fFecha = new Date(fechaS[0], --fechaS[1], fechaS[2]);
+    fFecha.setHours(0, 0, 0, 0);
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    var maxDate = new Date();
+    maxDate.setHours(0, 0, 0, 0);
+    maxDate.setYear(maxDate.getFullYear() + 20);
+    return (fFecha >= today) && (fFecha <= maxDate);
+}
+function format_time(date_obj) {
     // formats a javascript Date object into a 12h AM/PM time string
     var hour = date_obj.getHours() + 5;
     var minute = date_obj.getMinutes();
@@ -17,33 +29,40 @@ function format_date(date) {
     var formato = (date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + (date.getDate())).slice(-2));
     return formato;
 }
+var today = new Date();
 function agregaFuncion() {
     var fecha = $('#fechaFuncion').val();
     var date = new Date(fecha);
     var fechaF = format_date(date);
     var horaF = format_time(date);
     if (fecha) {
-        var rowId = $("#histFuncion").val();
-        var table = document.getElementById("bloqueFuncion");
-        var row = table.insertRow();
-        row.setAttribute("data-fecha", fechaF);
-        row.setAttribute("data-hora", horaF);
-        row.id = rowId;
-        var cell0 = row.insertCell(0);
-        cell0.innerHTML = parseInt(row.id) + 1;
-        var cell1 = row.insertCell(1);
-        cell1.innerHTML = fechaF;
-        var cell2 = row.insertCell(2);
-        cell2.innerHTML = horaF;
-        var cell3 = row.insertCell(3);
-        cell3.align = "center";
-        cell3.innerHTML = '<input type="radio" name="groupFunciones" value="' + (parseInt(row.id)) + '">';
+        if (validarFechaActualOMayor(fechaF)) {
+            var rowId = $("#histFuncion").val();
+            var table = document.getElementById("bloqueFuncion");
+            var row = table.insertRow();
+            row.setAttribute("data-fecha", fechaF);
+            row.setAttribute("data-hora", horaF);
+            row.id = rowId;
+            var cell0 = row.insertCell(0);
+            cell0.innerHTML = parseInt(row.id) + 1;
+            var cell1 = row.insertCell(1);
+            cell1.innerHTML = fechaF;
+            var cell2 = row.insertCell(2);
+            cell2.innerHTML = horaF;
+            var cell3 = row.insertCell(3);
+            cell3.align = "center";
+            cell3.innerHTML = '<input type="radio" name="groupFunciones" value="' + (parseInt(row.id)) + '">';
 
-        $("#fechaFuncion").val('');
-        $("#horaFuncion").val('');
+            $("#fechaFuncion").val('');
+            $("#horaFuncion").val('');
 
-        $("#fechaFuncion").focus();
-        $("#histFuncion").val(parseInt(row.id) + 1);
+            $("#fechaFuncion").focus();
+            $("#histFuncion").val(parseInt(row.id) + 1);
+        } else {
+            alert("Fecha de inicio incorrecta: " + fechaF + ". Ingrese una fecha valida (Rango de fecha: " + today.getFullYear() + " - " + (today.getFullYear() + 20) + ").");
+        }
+    } else {
+        alert("Campos Vacios. Ingrese nuevamente.");
     }
 }
 function eliminarFuncion() {
