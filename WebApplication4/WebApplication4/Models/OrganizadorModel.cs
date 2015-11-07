f0a967d5240c9ab5ef78781289fcf2e465a6ce01
ramonbv <simon81265@gmail.com>
2015-11-07 13:56:00 -0500

    mantenimientos models validaciones

--- WebApplication4/WebApplication4/Models/OrganizadorModel.cs
+++ WebApplication4/WebApplication4/Models/OrganizadorModel.cs
@@ -13,6 +13,7 @@ namespace WebApplication4.Models
         [Required]
         [EmailAddress]
         [Display(Name = "Correo Electronico: ")]
+        [StringLength(100, ErrorMessage = "Maxima longitud de 100 caracteres.")]
         public string Email { get; set; }
 
         [Required]
@@ -22,14 +23,20 @@ namespace WebApplication4.Models
 
         [Required]
         [Display(Name = "# Doc: ")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Numero de documento debe ser numerico.")]
+        [StringLength(20, ErrorMessage = "Maxima longitud de 20 caracteres.")]
         public string codDoc { get; set; }
 
         [Required]
+        [StringLength(50, ErrorMessage = "Maxima longitud de 50 caracteres.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nombre no debe ser alfanumerico.")]
         [Display(Name = "Nombre Organizador: ")]
         public string nombre { get; set; }
 
         [Required]
         [Display(Name = "Telefono")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Telefono Movil debe ser numerico.")]
+        [StringLength(20, ErrorMessage = "Maxima longitud de 20 caracteres.")]
         public string telefono { get; set; }
     }
 
