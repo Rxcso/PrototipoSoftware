1deb4c5ca0697b36543058ecf810ad52aac2e9db
samoelSarmiento <samoel.sarmiento@pucp.pe>
2015-11-07 20:27:16 -0500

    xd

---
+++ WebApplication4/WebApplication4/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    [Serializable]
+    public class Coment
+    {
+        public string nombre { get; set; }
+        public string contenido { get; set; }
+        public DateTime? fecha { get; set; }
+    }
+}
