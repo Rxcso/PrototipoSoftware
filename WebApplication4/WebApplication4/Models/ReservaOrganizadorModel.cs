eefd32684fc0c89bd5dc38e4e2e6569c97b90e46
R <g>
2015-11-13 10:14:18 -0500

    Reserva organizador

--- WebApplication4/WebApplication4/Models/ReservaOrganizadorModel.cs
+++ WebApplication4/WebApplication4/Models/ReservaOrganizadorModel.cs
@@ -8,10 +8,12 @@ namespace WebApplication4.Models
     public class ReservaOrganizadorModel
     {
         public int idEvento { get; set; }
+        public string nameEvento { get; set; }
         public int[] idFuncion{get;set;}
         public string[] nameFuncion { get; set; }
         public int[] idZona { get; set;}
         public string[] nameZona { get; set; }
+        public int[] idAsiento { get; set; }
         public string[] AsientoXFuncion{get;set;}
         public bool[] eliminar {get; set;}
     }
