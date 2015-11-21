02eeedbedb035be1807bb4857f7ba31512bd7904
Piereyed <piereyed@gmail.com>
2015-11-21 00:45:40 -0500

    date de jquery bien importado

--- WebApplication4/WebApplication4/App_Start/BundleConfig.cs
+++ WebApplication4/WebApplication4/App_Start/BundleConfig.cs
@@ -24,25 +24,25 @@ namespace WebApplication4
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js"));
+            "~/Scripts/jquery-ui-{version}.js",
+            "~/Scripts/datepicker-es.js")); //espanol del datepicker
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-              "~/Content/themes/base/jquery.ui.core.css",
-              "~/Content/themes/base/jquery.ui.resizable.css",
-              "~/Content/themes/base/jquery.ui.selectable.css",
-              "~/Content/themes/base/jquery.ui.accordion.css",
-              "~/Content/themes/base/jquery.ui.autocomplete.css",
-              "~/Content/themes/base/jquery.ui.button.css",
-              "~/Content/themes/base/jquery.ui.dialog.css",
-              "~/Content/themes/base/jquery.ui.slider.css",
-              "~/Content/themes/base/jquery.ui.tabs.css",
-              "~/Content/themes/base/jquery.ui.datepicker.css",
-              "~/Content/themes/base/jquery.ui.progressbar.css",
-              "~/Content/themes/base/jquery.ui.theme.css"));
+              "~/Content/themes/base/core.css",
+              "~/Content/themes/base/resizable.css",
+              "~/Content/themes/base/selectable.css",
+              "~/Content/themes/base/accordion.css",
+              "~/Content/themes/base/autocomplete.css",
+              "~/Content/themes/base/button.css",
+              "~/Content/themes/base/dialog.css",
+              "~/Content/themes/base/slider.css",
+              "~/Content/themes/base/tabs.css",
+              "~/Content/themes/base/datepicker.css",
+              "~/Content/themes/base/progressbar.css",
+              "~/Content/themes/base/theme.css"));
         }
     }
 }
