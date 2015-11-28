3c1d6eb3f24ef6aa46476ea0421f032733caf272
Piereyed <piereyed@gmail.com>
2015-11-28 16:04:53 -0500

    todos los reportes con fechas jqueryUI

--- WebApplication4/WebApplication4/Scripts/script_fecha.js
+++ WebApplication4/WebApplication4/Scripts/script_fecha.js
@@ -4,21 +4,35 @@ $(function () {
 
     $(".fecha").datepicker({
         format: "dd-mm-yy",
-        altFormat: "yy-mm-dd",        
+        altFormat: "yy-mm-dd",
         minDate: "-30D",
         maxDate: "+12M",
         showAnim: "fade",
         changeYear: true
-    });    
+    });
 
     $("#fech_ini").datepicker("option", "altField", "#altfech_ini");
-    $("#fech_fin").datepicker("option", "altField", "#altfech_fin");   
+    $("#fech_fin").datepicker("option", "altField", "#altfech_fin");
 
-     
-    $(".fecha2").datepicker({        
-        format: "dd-mm",        
+
+    $(".fecha2").datepicker({
+        format: "dd-mm",
         maxDate: "+0D",
         showAnim: "fade",
         changeYear: true
     });
+
+
+    //para index 2
+
+    $(".fechar").datepicker({
+        dateFormat: "yy/mm/dd",
+        //altFormat: "yy-mm-dd",
+        minDate: "-30D",
+        maxDate: "+12M",
+        showAnim: "fade",
+        changeYear: true
+    });
+
+
 });
