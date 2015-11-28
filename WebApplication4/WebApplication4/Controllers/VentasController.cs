ae277062b47916aa704040b0df044d427e25066d
Sephirihos <sephirihos18@gmail.com>
2015-11-28 00:31:56 -0500

    correcion de algunos errores

--- WebApplication4/WebApplication4/Controllers/VentasController.cs
+++ WebApplication4/WebApplication4/Controllers/VentasController.cs
@@ -5,6 +5,8 @@ using System.Data.Entity;
 using System.Data.Entity.Core;
 using System.Data.Entity.Validation;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
