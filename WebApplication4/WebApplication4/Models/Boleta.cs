ef8c4577710e2c9407fd37f6c08fe24d0dfb305f
Sephirihos <sephirihos18@gmail.com>
2015-11-29 12:48:27 -0500

    mas cambios

---
+++ WebApplication4/WebApplication4/Models/Boleta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Boleta
+    {
+
+        public double Total { get; set; }
+        public List<DetalleVentaBoleta> detalles { get; set; }
+    }
+}
---
+++ WebApplication4/WebApplication4/Models/DetalleVentaBoleta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class DetalleVentaBoleta
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int? Cantidad { get; set; }
+        public string Zona { get; set; }
+        public double? Total { get; set; }
+        public double? Precio { get; set; }
+
+
+    }
+}
